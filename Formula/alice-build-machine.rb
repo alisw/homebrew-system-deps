b2294d1e60b98090ff77969209884ee57b7b6b97
Sergio Garcia <sergio.garcia.de.la.cruz@cern.ch>
2025-08-13 11:39:11 +0200

    Fix `brew audit` in formulas and add a checker to CI

--- Formula/alice-build-machine.rb
+++ Formula/alice-build-machine.rb
@@ -2,17 +2,17 @@ class AliceBuildMachine < Formula
   desc "ALICE O2 Build Machine Dependencies via Homebrew"
   homepage "https://alisw.github.io"
   url "file:///dev/null"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "23.48-1"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "alisw/system-deps/o2-full-deps"
-  depends_on "openjdk"
-  depends_on "hashicorp/tap/nomad"
-  depends_on "hashicorp/tap/consul"
-  depends_on "jq"   # for build scripts
   depends_on "git"
+  depends_on "hashicorp/tap/consul"
+  depends_on "hashicorp/tap/nomad"
+  depends_on "jq" # for build scripts
+  depends_on "openjdk"
 
   def install
-    system "touch", "#{prefix}/empty"
+    touch "#{prefix}/empty"
   end
 end
