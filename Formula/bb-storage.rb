ac93c82de91274c6d2a02fece554bdf91a41d456
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-01-07 16:14:40 +0100

    Add formula for bb-storage

---
+++ Formula/bb-storage.rb
@@ -0,0 +1,29 @@
+class BuildBarnStorage < Formula
+  desc "BuildBarnStorage Daemon"
+  homepage "https://github.com/buildbarn/bb-storage"
+  url "file:///dev/null"
+  url "https://github.com/buildbarn/bb-storage"
+      revision: "51e1a67922e22d65445f43aa809636cb481644f3"
+  version "2026-01-07"
+
+  depends_on "bazelisk"
+
+  def install
+    bazel_args = %W[
+      --jobs=#{ENV.make_jobs}
+      --compilation_mode=opt
+      --copt=#{optflag}
+      --linkopt=-Wl,-rpath,#{rpath}
+      --verbose_failures
+    ]
+    targets = %w[
+      cmd/bb_storage/:bb_storage
+    ]
+    system Formula["bazelisk"].opt_bin/"bazelisk", "build", *bazel_args, *targets
+
+    bin.install %w[
+      bazel-bin/cmd/bb_storage/bb_storage
+    ]
+
+  end
+end
