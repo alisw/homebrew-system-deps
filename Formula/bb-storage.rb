1492cf87fdf90d347729136904c0b4644f616400
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-01-08 09:12:19 +0100

    bb-storage working version

--- Formula/bb-storage.rb
+++ Formula/bb-storage.rb
@@ -1,25 +1,28 @@
 class BbStorage < Formula
   desc "BuildBarnStorage Daemon"
   homepage "https://github.com/buildbarn/bb-storage"
-  url "https://github.com/buildbarn/bb-storage",
+  url "https://github.com/buildbarn/bb-storage.git",
       revision: "51e1a67922e22d65445f43aa809636cb481644f3"
   version "2026-01-07"
 
-  depends_on "bazelisk"
+  depends_on "bazelisk" => :build
+  depends_on "go" => :build
 
   def install
+    ENV["CC"] = "/usr/bin/clang"
+    ENV["CXX"] = "/usr/bin/clang++"
+
     bazel_args = %W[
       --jobs=#{ENV.make_jobs}
       --compilation_mode=opt
       --linkopt=-Wl,-rpath,#{rpath}
       --verbose_failures
+      --repo_contents_cache=
     ]
     targets = %w[
-      //cmd/bb_storage/:bb_storage
+      //cmd/bb_storage:bb_storage
     ]
-    cd "bb-storage" do
-      system Formula["bazelisk"].opt_bin/"bazelisk", "build", *bazel_args, *targets
-    end
+    system Formula["bazelisk"].opt_bin/"bazelisk", "build", *bazel_args, *targets
 
     bin.install %w[
       bazel-bin/cmd/bb_storage/bb_storage_/bb_storage
